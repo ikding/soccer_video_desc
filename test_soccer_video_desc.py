05c8e145127ba6e1dcc70245ba9ce30f46b1e499
I-Kang Ding <ikding@gmail.com>
2024-01-27 21:48:00 -0800

    Add test for both example yaml files

--- test_soccer_video_desc.py
+++ test_soccer_video_desc.py
@@ -16,14 +16,13 @@ def test_parse_timestamp():
     assert parse_timestamp("00:45") == "00:45"
 
 
-def test_soccer_game_description():
+def test_soccer_game_description_regular_season():
     with open(
         Path(__file__).parent.resolve() / "examples" / "regular_season.yaml", "r"
     ) as f:
         game = yaml.safe_load(f)
 
     description = soccer_game_description(**game)
-    print(description)
     assert "\n" + description + "\n" == """
 Bay Area Surf 13B Pre-MLS 6-2 Palo Alto SC 12B Gold | Norcal U12 Premier | 2023-11-04
 
@@ -39,4 +38,25 @@ Bay Area Surf 13B Pre-MLS 6-2 Palo Alto SC 12B Gold
 10:03 Bay Area Surf 5-[1] Palo Alto SC - #27 (assist from #70)
 11:02 Bay Area Surf 5-[2] Palo Alto SC - #90 (assist from #6)
 11:32 Bay Area Surf [6]-2 Palo Alto SC - Dominic
+"""
+
+
+def test_soccer_game_description_tournament():
+    with open(
+        Path(__file__).parent.resolve() / "examples" / "tournament.yaml", "r"
+    ) as f:
+        game = yaml.safe_load(f)
+
+    description = soccer_game_description(**game)
+    assert "\n" + description + "\n" == """
+Bay Area Surf 13B Pre-MLS 4-0 CVFA 13B I | LV Mayor's Cup U11 1st Div | Final | 2023-10-29
+
+2023-10-29 LV Mayor's Cup U11 1st Div (Final)
+
+Bay Area Surf 13B Pre-MLS 4-0 CVFA 13B I
+
+3:23 Bay Area Surf [1]-0 CVFA - Eli (Free Kick)
+4:26 Bay Area Surf [2]-0 CVFA - Galvan (assist from Alexander)
+7:58 Bay Area Surf [3]-0 CVFA - Dominic (assist from Galvan)
+9:04 Bay Area Surf [4]-0 CVFA - Dominic (assist from Alexander)
 """
