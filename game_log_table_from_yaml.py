70a44d7286b91a8e74ce254ea02ac8209690c976
I-Kang Ding <ikding@gmail.com>
2024-03-23 10:01:02 -0700

    Refactor game log parsing and add timestamp parsing

--- game_log_table_from_yaml.py
+++ game_log_table_from_yaml.py
@@ -3,37 +3,52 @@ import logging
 from collections import defaultdict
 from pathlib import Path
 
-import pandas as pd
+import pandas as pd  # type: ignore
 import yaml
-from tqdm.auto import tqdm
+from tqdm.auto import tqdm  # type: ignore
+from video_desc_from_yaml import parse_timestamp
 
 logger = logging.getLogger(__name__)
 
 
-def load_logs_to_dataframe(logs: list[dict]) -> pd.DataFrame:
+def parse_log_files_to_dataframe(yaml_files: list[Path]) -> pd.DataFrame:
     """
     Converts a list of game logs to a pandas DataFrame.
 
     Args:
-        logs (list[dict]): A list of game logs, where each log is a dictionary.
+        yaml_files (list[Path]): A list of yaml file paths containing game logs.
 
     Returns:
         pd.DataFrame: A DataFrame containing the game logs.
     """
     logs_dict = defaultdict(list)
 
-    for log in tqdm(logs, desc="games"):
-        if "goals" in log:
-            for goal in log["goals"]:
-                logs_dict["date"].append(log["date"])
-                logs_dict["division"].append(log["division"])
-                logs_dict["round"].append(log["round"])
-                logs_dict["home_team"].append(log["home_team"])
-                logs_dict["away_team"].append(log["away_team"])
-                logs_dict["scoring_team"].append(goal["scoring_team"])
+    for yaml_file in tqdm(yaml_files):
+        if yaml_file.suffix != ".yaml":
+            continue
+        with open(yaml_file, "r") as f:
+            try:
+                game_log = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                logger.error(e)
+                raise e
+
+        if "goals" in game_log:
+            for goal in game_log["goals"]:
+                logs_dict["file_name"].append(yaml_file.stem)
+                logs_dict["date"].append(game_log["date"])
+                logs_dict["division"].append(game_log["division"])
+                logs_dict["round"].append(game_log["round"])
+                logs_dict["home_team"].append(game_log["home_team"])
+                logs_dict["away_team"].append(game_log["away_team"])
+                logs_dict["scoring_team"].append(
+                    game_log["home_team"]
+                    if goal["scoring_team"] == "H"
+                    else game_log["away_team"]
+                )
                 logs_dict["scoring_player"].append(goal["scoring_player"])
                 logs_dict["assist_player"].append(goal.get("assist_player", None))
-                logs_dict["minute"].append(goal.get("minute", None))
+                logs_dict["timestamp"].append(parse_timestamp(goal["timestamp"]))
 
     return pd.DataFrame(logs_dict)
 
@@ -48,19 +63,5 @@ if __name__ == "__main__":
 
     yaml_dir_path = Path(args.yaml_dir)
 
-    game_logs_list = []
-    for yaml_file in yaml_dir_path.iterdir():
-        if yaml_file.suffix != ".yaml":
-            continue
-        with open(yaml_file, "r") as f:
-            try:
-                game_logs = yaml.safe_load(f)
-            except yaml.YAMLError as e:
-                logger.error(e)
-                raise e
-
-            game_logs_list.append(game_logs)
-
-    df = load_logs_to_dataframe(game_logs_list)
-
-    print(df)
+    df = parse_log_files_to_dataframe(sorted(list(yaml_dir_path.iterdir())))
+    df.to_csv(yaml_dir_path / "game_logs.csv", index=False)
